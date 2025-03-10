7ffc0a5392a678858ee8eadea4ce7d283a4de58a
Kithmi Hettiarachchi <kithmi0420@gmail.com>
2025-03-11 01:14:46 +0530

    Updated configuration

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin: '*',
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization']
   }));
@@ -15,5 +15,7 @@ app.use(cors({
 app.use(express.json());
 app.use('/api', searchRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+console.log("Elasticsearch URL:", process.env.ELASTICSEARCH_URL);
+console.log("Elasticsearch Index:", process.env.ELASTICSEARCH_INDEX);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
