d6ad98dc22aee6c6fa99d9d48153809ca7f27f3f
Harini Nandasens <harininandasena0529@gmail.com>
2025-03-19 13:29:33 +0530

    port changes

--- src/Server.js
+++ src/Server.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 import dataSourceRoutes from './routes/dataSourceRoutes.js';
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(bodyParser.json());
