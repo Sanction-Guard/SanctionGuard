01600a7f231587a3ce89092d33b6f2f9f6c63b16
Lakindu Karunanayake <lakindu.20231358@iit.ac.lk>
2025-03-23 23:42:23 +0530

    Cleaning

--- src/routes/auditRoutes.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import searchController from '../controllers/searchController.js';
-const router = express.Router();
-router.get('/audit-logs', searchController.getAuditLogs);
-
-export default router;
